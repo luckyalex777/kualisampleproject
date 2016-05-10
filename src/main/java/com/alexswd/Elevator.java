c205450644b182a8ca87bf9f4c7da3c2540a84b5
alex <alex@udev.home.local>
2016-05-10 16:12:43 -0400

    in progress

--- src/main/java/com/alexswd/Elevator.java
+++ src/main/java/com/alexswd/Elevator.java
@@ -1,4 +1,44 @@
 package com.alexswd;
 
-public class Elevator {
+
+public class Elevator implements Runnable {
+    private int id;
+    private int maxFloor;
+    private int currentFloor;
+    private Trip trip;
+    private boolean occupped;
+    private boolean shutdownRequested;
+
+    public Elevator(int id, int maxFloor) {
+        this.id = id;
+        this.maxFloor = maxFloor;
+        currentFloor = 1;
+    }
+
+    private void reportStatus() {
+
+    }
+    public void doTrip(int fromFloor, int toFloor) {
+        if( this.trip == null ) {
+            this.trip = new Trip(fromFloor, toFloor);
+        }
+    }
+    public void shutdown() {
+        shutdownRequested = true;
+    }
+
+    @Override
+    public void run() {
+        while( true ) {
+            try {
+                Thread.sleep(1000);
+                if( this.trip != null ) {
+                    // do staff
+                }
+            }
+            catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+    }
 }
